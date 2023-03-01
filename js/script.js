846cc620a0fd2c8fe6b14f7928211b47d3aed2d6
Matheus Masera <matheusmasera2@gmail.com>
2023-02-28 21:03:55 -0300

    pegando parametros da URL

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Ativar links do menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -10,3 +11,20 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// Ativar items do orçamento
+
+const parametros = new URLSearchParams(location.search);
+console.log(parametros);
+
+//função ativada para cada elemento definido no const parametros e no forEach
+function ativarProduto(parametro) {
+  //pega o id de cada location.search e seu respectivo elemento: o input
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+//ativa um loop para cada parametro no urlsearchparams
+parametros.forEach(ativarProduto);
