d45fe3e59ddec29b4647ebf391bf0b59991ea7d2
Matheus Masera <matheusmasera2@gmail.com>
2023-03-02 22:42:26 -0300

    galeria bicicleta criada

--- js/script.js
+++ js/script.js
@@ -15,7 +15,6 @@ links.forEach(ativarLink);
 // Ativar items do orçamento
 
 const parametros = new URLSearchParams(location.search);
-console.log(parametros);
 
 //função ativada para cada elemento definido no const parametros e no forEach
 function ativarProduto(parametro) {
@@ -28,3 +27,42 @@ function ativarProduto(parametro) {
 
 //ativa um loop para cada parametro no urlsearchparams
 parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", "true");
+
+  console.log(resposta);
+}
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function eventoPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventoPerguntas);
+
+// Galeria de Bicicleta
+
+const galeria = document.querySelectorAll(".bicicleta-img img");
+const galeriaContainer = document.querySelector(".bicicleta-img");
+
+function eventosGaleria(img) {
+  img.addEventListener("click", (e) => {
+    const img = e.currentTarget;
+    const media = matchMedia("(min-width: 1000px)").matches;
+    if (media) {
+      galeriaContainer.prepend(img);
+    }
+  });
+}
+
+galeria.forEach(eventosGaleria);
